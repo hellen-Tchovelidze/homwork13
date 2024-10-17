1dadc2e9c4d4ed61e1a2565ea25da143fa96fadd
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-10-17 19:52:31 +0400

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const name = "hellen";
+const lastname = "tchovelidze";
+let age = 23;
+const email = "elenechovelidze@gmail.com";
+const phone = 592011069;
+const adultBoolean = true;
+
+console.log(name);
+console.log(lastname);
+console.log(age);
+console.log(email);
+console.log("%c592011069", "color:red");
+console.log("%ctrue", "color:yellow");
